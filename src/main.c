87a9ecbdca417387938af07c3e0eba9b371e3f9d
Giovanna Garcia <ggarciabas@gmail.com>
2016-05-29 18:44:28 -0300

    Estrutura de arquivos.
    Estrutura dos arquivos .c .h, main definida. Exemplo de janela desejada.

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+	Giovanna Garcia Basilio : ggarciabas@gmail.com
+	Gian Maurício Fritsche : gian.fritsche@gmail.com
+*/
+
+#include "include.h"
+
+int main () {
+
+	printf("Hello World\n");
+
+	return 0;
+}
